ede1f473489faa96654b47f6dbcf6a44f30bdb9f
2405-011-pw <walypa56@gmail.com>
2024-06-06 15:36:42 +0100

    made fizzBuzzEnquirer

---
+++ src/enquirerDemo/paulProject/fizzBuzzEnquirer.js
@@ -0,0 +1,24 @@
+const { prompt } = require("enquirer");
+
+prompt({
+    type: "input",
+    message: "What is your fizzBuzz number?",
+    name: "fizzBuzzNumber",
+}).then(generateFizzBuzz);
+
+function generateFizzBuzz(num) {
+    let fizzBuzzArray = [];
+    for (let i = 1; i <= num.fizzBuzzNumber; i++) {
+      if (i%3 === 0 && i%5 === 0) {
+        fizzBuzzArray.push("FizzBuzz");
+      } else if (i%3 === 0) {
+        fizzBuzzArray.push("Fizz");
+      } else if (i%5 === 0) {
+        fizzBuzzArray.push("Buzz");
+      } else {
+        fizzBuzzArray.push(i);
+      }
+    }
+    console.log("Your fizzBuzzArray = " + fizzBuzzArray);
+    return fizzBuzzArray;
+  }
