7d41514c44681f586caad636bedcf0f328ee4639
2405-011-pw <walypa56@gmail.com>
2024-06-07 14:24:11 +0100

    search for simpsons episodes

--- src/axiosDemo/axiosAttemptPaul.js
+++ src/axiosDemo/axiosAttemptPaul.js
@@ -1,11 +1,25 @@
+const { prompt } = require("enquirer");
 const axios = require("axios").default;
 
-async function mainTask() {
+prompt({
+    type: "input",
+    message: "Give me information to find your Simpsons episode?",
+    name: "key",
+}).then(mainTask);
+
+async function mainTask(search) {
     const result = await axios.get("https://api.tvmaze.com/shows/83/episodes");
-    console.log("GET completed.  Result object includes: ");
-    console.log(result.status[0]);
-    console.log(result.data[0]);
+    let relevantData = filterData(result.data, search.key)
+    console.log("Is your episode one of these: ");
+    for (let i of relevantData){
+        console.log(i.name);
+    }
 }
 
-mainTask();
-console.log("after main task");
+function filterData(data, searchKey) {  //Function filters for objects that contain values
+    return data.filter(item => {        //that are strings and include the user prompt
+        return Object.values(item).some(value => 
+            typeof value === 'string' && value.includes(searchKey)
+        );
+    });
+}
